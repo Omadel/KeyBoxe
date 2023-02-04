fdcbf5fa2195b32bfbd581ea0cc30bf07956dded
Etienne Javelle <83096541+Omadel@users.noreply.github.com>
2023-02-04 15:09:52 +0100

    Knock Out

--- Assets/_Project/Scripts/Runtime/Player.cs
+++ Assets/_Project/Scripts/Runtime/Player.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using DG.Tweening;
+using System.Collections;
 
 namespace Route69
 {
@@ -11,9 +12,10 @@ namespace Route69
         [SerializeField] string playerName;
         [SerializeField] int startHealth = 30;
         [SerializeField] int attackDamage = 3;
-        [SerializeField] int stability = 30;
+        [SerializeField] Color hitColor = Color.black;
 
         Animator animator;
+        Tween hitTween;
 
         private void Start()
         {
@@ -45,12 +47,28 @@ namespace Route69
         private void Attack()
         {
             var pushDistance = GameManager.Instance.CurrentBoss.Hit(attackDamage);
+            if (pushDistance<=-1)
+            {
+                StartCoroutine(Win());
+                return;
+            }
             transform.DOMoveZ(transform.position.z + pushDistance, .4f).SetDelay(.2f);
+        }
+
+        private IEnumerator Win()
+        {
+            yield return new WaitForSeconds(2f);
+            animator.Play("Victory");
         }
 
         public void Hit(int damage, float push)
         {
             SetHealth(currentHealth - damage);
+            animator.Play("Hit", 0, 0f);
+            var material = animator.GetComponentInChildren<Renderer>().material;
+            const string colorName = "_FillColor";
+            material.SetColor(colorName, hitColor);
+            hitTween = DOTween.ToAlpha(() => material.GetColor(colorName), c => material.SetColor(colorName, c), 0f, .4f);
             transform.DOMoveZ(transform.position.z - push, .4f).SetEase(Ease.OutCirc);
         }
     }
