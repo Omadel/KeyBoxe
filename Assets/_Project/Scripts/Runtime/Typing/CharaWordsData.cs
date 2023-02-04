b338efcbf4fe48473807f53ebc0ed520d7c466ac
GodgrEey <114407159+GodgrEey@users.noreply.github.com>
2023-02-04 19:06:10 +0100

    ro-beau

---
+++ Assets/_Project/Scripts/Runtime/Typing/CharaWordsData.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Route69
+{
+    [CreateAssetMenu(fileName = "New Chara Word", menuName = "Route69/Chara Words Data")] 
+    public class CharaWordsData : ScriptableObject
+    {
+        public WordsToTypePhase[] WordsToType;
+        public float[] SpawnWordsRatePerPhase;
+        public float[] TimeForNewPhase;
+
+        public float[] WordSpeedPerPhase;
+        public int[] LifeDamagePerPhase;
+        public float[] PushDamagePerPhase;
+
+        [System.Serializable]
+        public struct WordsToTypePhase
+        {
+            public string[] Words;
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            foreach (var item in WordsToType)
+            {
+                for (int i = 0; i < item.Words.Length; i++)
+                {
+                    item.Words[i] = item.Words[i].ToUpper();
+                    item.Words[i] = String.Concat(item.Words[i].Where(c => !Char.IsWhiteSpace(c)));
+                }
+            }
+        }
+#endif
+    }
+}
