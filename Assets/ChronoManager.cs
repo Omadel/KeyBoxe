d9a9b0833f905246ce0e7c8525d7e3f570416467
Samuel <49845876+MikiSaxo@users.noreply.github.com>
2023-02-05 01:08:21 +0100

    Time Out

--- Assets/ChronoManager.cs
+++ Assets/ChronoManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using TMPro;
 using UnityEngine.PlayerLoop;
@@ -10,6 +11,7 @@ namespace Route69
     public class ChronoManager : Etienne.Singleton<ChronoManager>
     {
         [SerializeField] private TextMeshProUGUI _chronoText;
+        [SerializeField] private GameObject _timeOut;
         private double _actualChrono;
 
         private void Start()
@@ -37,8 +39,26 @@ namespace Route69
             format = format.Substring(0, 5);
             _chronoText.text = format;
 
-            if (_actualChrono < 5)
+            if (_actualChrono < 15)
                 _chronoText.color = Color.red;
+            
+            if(_actualChrono <= 0)
+                LaunchTimeOut();
+        }
+
+        private void LaunchTimeOut()
+        {
+            StartCoroutine(WaitToDespawn());
+        }
+        
+        private IEnumerator WaitToDespawn()
+        {
+            _timeOut.transform.DOScale(0, 0);
+            _timeOut.transform.DOScale(1, .5f);
+            yield return new WaitForSeconds(2);
+            _timeOut.transform.DOScale(1.2f, .1f);
+            yield return new WaitForSeconds(.2f);
+            _timeOut.transform.DOScale(0, 4f);
         }
     }
 }
